a6df9c607072593d292b10dffe7333c1a53c5722
Bartek <shox12.47@gmail.com>
2020-08-15 17:41:18 +0200

    Updates

---
+++ precache-manifest.9b9dcd01c9431a492686d507d41552f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96069d6ed0c612954e0629849305de7a",
+    "url": "/Weather-APP/index.html"
+  },
+  {
+    "revision": "924ad0552963df96322f",
+    "url": "/Weather-APP/static/js/2.bef0cd34.chunk.js"
+  },
+  {
+    "revision": "e23d72e353070f497294c1592aa53554",
+    "url": "/Weather-APP/static/js/2.bef0cd34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a9610213ffd6309eee",
+    "url": "/Weather-APP/static/js/main.c64fcb4e.chunk.js"
+  },
+  {
+    "revision": "95412b0ad9f90527ef92",
+    "url": "/Weather-APP/static/js/runtime-main.114c3c7d.js"
+  }
+]);
